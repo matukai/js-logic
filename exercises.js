76401740d1e267af0fa271792bf04fa378a4f860
matukai <matt.tanji@gmail.com>
2017-09-11 22:57:26 -1000

    must be true

--- exercises.js
+++ exercises.js
@@ -29,6 +29,14 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * The function will return true if the number passed into the function is equal to or greater than Hawaii's voting age. Console.log your result.
 */
 
+function canVote(age){
+	if(age >= 18){
+		return true;
+	}else{
+		return false;
+	}
+}
+console.log(canVote(17));
 
 /*
  * #2
@@ -42,6 +50,14 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * Console.log your result.
 */
 
+function login(password){
+	if(password === "test1234"){
+		return "Login Successful";
+	}else{
+		return "Login Failed";
+	}
+}
+console.log(login("test1234"));
 
 /*
  * #3
@@ -56,6 +72,14 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * Console.log your result.
 */
 
+function isGreaterTan(first,second){
+	if(first > second){
+		return true;
+	}else{
+		return false;
+	}
+}
+console.log(isGreaterTan(1,100));
 
 
 /*
@@ -69,7 +93,14 @@ If/else statements = Evaluates (or checks) a condition. If the condition is true
  * The function will return true if the value passed into the function is "true".
  * Console.log your result.
 */
-
+function mustBeTrue(boo){
+	if(true === true){
+		return true;
+	}else{
+		return false;
+	}
+}
+console.log(mustBeTrue(true));
 
 
 /*
